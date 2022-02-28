5bd67effb74f403d4fa2ac1ee1a7c28a031ac449
Josue <josuepereztorr@gmail.com>
2022-02-27 20:01:58 -0600

    Created the basic structure of the loop, if the user inputs 1 the program will create a Tickets.csv and if the user chooses 2 then the user will be prompted to add a new record.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace TicketApplication
 {
@@ -6,7 +7,29 @@ namespace TicketApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string file = "Tickets.csv";
+            string choice;
+            StreamWriter sw;
+
+            Console.WriteLine("\nTicketing System\n" + new String('-', 16));
+
+            do
+            {
+                Console.WriteLine("\n1) Create a Tickets.csv file from data.");
+                Console.WriteLine("2) Add a new record.");
+                Console.WriteLine("\nOr enter any other key to exit.");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        Console.WriteLine("Chose 2");break;
+                }
+            } while (choice is "1" or "2");
+            
         }
     }
 }
