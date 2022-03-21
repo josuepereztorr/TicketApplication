bf904d551db3512bbe9d8599029efb0b2bce576a
Josue <josuepereztorr@gmail.com>
2022-03-20 20:03:20 -0500

    Fixed filename defects varible

--- Tickets/Filename.cs
+++ Tickets/Filename.cs
@@ -2,7 +2,7 @@ namespace TicketApplication.Tickets
 {
     public static class Filename 
     {
-        public static readonly string Tickets = "Tickets.csv";
+        public static readonly string Defects = "Tickets.csv";
         public static readonly string Enhancements = "Enhancements.csv";
         public static readonly string Tasks = "Tasks.csv";
     }
